06ac4ccbf487833aa42850f73abc138bb1188715
Scott W <scottaza@gmail.com>
2021-12-28 13:02:27 -0700

    Fix for marketing app deployment exposes option

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -17,7 +17,7 @@ const prodConfig = {
             name: 'container',
             filename: 'remoteEntry.js',
             exposes: {
-                './MarketingApp': './src/bootsrtap'
+                './MarketingApp': './src/bootstrap'
             },
             shared: packageJson.dependencies
         })
